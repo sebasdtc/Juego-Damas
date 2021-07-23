181c3c5c15c3b3fd2c0a107dfe62e2dccc324840
Sebastian Torres <sebasdtc@outlook.com>
2021-07-22 23:52:17 -0500

    se creo la clase jugador

--- Player.cs
+++ Player.cs
@@ -4,4 +4,34 @@ class Player
 {
     public string Name { get; set; }
 
+    public ConsoleColor Tag { get; set; }
+
+    public int PosX { get; set; }
+
+    public int PosY { get; set; }
+
+    public bool SelectPiece(int row, int col)
+    {
+        if ( Tag == Board.GetPiece(row, col).Color )
+        {
+            PosX = col;
+            PosY = row;
+            Board.GetPiece(row, col).Background = ConsoleColor.Yellow;
+            return true;
+        }
+        return false;
+    }
+
+    public bool MovePiece(int row, int col)
+    {
+        if (Tag == ConsoleColor.Red) 
+        {
+            
+        }
+        else
+        {
+
+        }
+        return false;
+    }
 }
